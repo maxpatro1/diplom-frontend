c0a5d5a093336cc09aca6bc9323b6c1a65a7ee6f
Daniil Denisov <Cap.den2001@gmail.com>
2023-01-28 18:53:21 +0300

    added page title param

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from '@/router/routes';
 
+const DEFAULT_TITLE = 'default page title';
+
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.afterEach((to) => {
+  document.title = to.meta.title || DEFAULT_TITLE;
 });
 
 export default router;
