5949cc25ec75222a5aebf4faf26da3f6782f76fb
Daniil Denisov <Cap.den2001@gmail.com>
2023-01-30 22:06:11 +0300

    added BaseInput

---
+++ src/components/base/BaseInput.vue
@@ -0,0 +1,67 @@
+<template>
+  <label :class="classes">
+    <span v-if="label" class="base-input__label">{{ label }}</span>
+    <input
+      :value="modelValue"
+      :type="type"
+      class="base-input__input"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+    modelValue: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    isGrayLabel: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: 'text'
+    }
+  },
+  computed: {
+    classes() {
+      return [
+        'base-input',
+        {
+          'gray-label': this.isGrayLabel,
+        }
+      ];
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.base-input {
+  width: 100%;
+
+  &.gray-label {
+    color: $grey-primary;
+  }
+
+  &__label {
+    display: block;
+    margin-bottom: 8px;
+  }
+
+  &__input {
+    width: 100%;
+    padding: 8px;
+    border: 1px solid $blue-primary;
+    border-radius: $radius-sm;
+  }
+}
+</style>
