7e161b4260fb0834f1211bd728e970606885e49f
Daniil Denisov <Cap.den2001@gmail.com>
2023-01-30 22:08:05 +0300

    added BaseLink

---
+++ src/components/base/BaseLink.vue
@@ -0,0 +1,34 @@
+<template>
+  <router-link v-if="to" class="base-link" :to="to">
+    <slot></slot>
+  </router-link>
+  <a v-else class="base-link" :href="href">
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'BaseLink',
+  props: {
+    href: {
+      type: String,
+      default: ''
+    },
+    to: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.base-link {
+  color: $blue-primary;
+
+  &:hover {
+    text-decoration: underline;
+  }
+}
+</style>
