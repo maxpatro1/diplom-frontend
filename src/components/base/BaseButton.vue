7764006beca72f6a6ea2fe72467bed4239d217c3
Daniil Denisov <Cap.den2001@gmail.com>
2023-01-30 22:05:51 +0300

    added BaseButton

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="base-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+};
+</script>
+
+<style lang="scss" scoped>
+.base-button {
+  padding: 8px 24px;
+  color: $light;
+  background: $blue-primary;
+  border-radius: $radius-sm;
+}
+</style>
