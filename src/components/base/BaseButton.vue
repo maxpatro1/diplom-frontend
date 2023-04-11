2d00a8c497af4b688e975aed70323ac03e5b896e
Daniil Denisov <Cap.den2001@gmail.com>
2023-04-11 14:27:32 +0300

    update BaseButton

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button :class="classes" :disabled="isLoading">
-    <base-circle-loader v-if="isLoading" />
+    <base-circle-loader v-if="isLoading" :color="loaderColor" />
     <slot v-else></slot>
   </button>
 </template>
@@ -8,9 +8,16 @@
 <script>
 import BaseCircleLoader from '@/components/base/BaseCircleLoader.vue';
 
+const COLORS = ['blue', 'light', 'danger'];
+
 export default {
   name: 'BaseButton',
   props: {
+    color: {
+      type: String,
+      default: 'blue',
+      validator: (value) => COLORS.includes(value)
+    },
     isLoading: {
       type: Boolean,
       default: false
@@ -20,9 +27,20 @@ export default {
     BaseCircleLoader,
   },
   computed: {
+    loaderColor() {
+      switch (this.color) {
+        case COLORS[1]:
+          return 'dark'
+        case COLORS[2]:
+          return 'danger'
+        default:
+          return 'light'
+      }
+    },
     classes() {
       return [
         'base-button',
+        `color-${this.color}`,
         {
           ['is-loading']: this.isLoading
         }
@@ -38,15 +56,27 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
-  color:#fff;
   font-weight: 400;
-  background: $dark-blue;
   border-radius: 16px;
   cursor: pointer;
   transition: $transition-md;
 
   &:hover {
     opacity: .5;
+  }
+
+  &.color-blue {
+    background: $dark-blue;
+    color:#fff;
+  }
+
+  &.color-light {
+    background: $honey-dew;
+  }
+
+  &.color-danger {
+    background: $honey-dew;
+    color: $signal-red;
   }
 
   &.is-loading {
