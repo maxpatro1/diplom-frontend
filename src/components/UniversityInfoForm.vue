2b0329ac41092a7e28d7daff85a0a56a32b544e9
Daniil Denisov <Cap.den2001@gmail.com>
2023-04-12 15:15:18 +0300

    add group field to universityInfoForm

--- src/components/UniversityInfoForm.vue
+++ src/components/UniversityInfoForm.vue
@@ -1,23 +1,37 @@
 <template>
   <v-form class="university" @submit="submit">
     <h1 class="university__title">Учебное заведение</h1>
+    <base-input
+      v-model="group"
+      name="group"
+      type="text"
+      placeholder="Введите номер группы"
+    />
     <base-button type="submit">Зарегистрироваться</base-button>
   </v-form>
 </template>
 
 <script>
 import BaseButton from '@/components/base/BaseButton.vue';
+import BaseInput from '@/components/base/BaseInput.vue';
 import { Form } from 'vee-validate';
 
 export default {
   name: 'UniversityInfoForm',
   components: {
     BaseButton,
+    BaseInput,
     VForm: Form
+  },
+  data() {
+    return {
+      group: ''
+    };
   },
   methods: {
     submit() {
       this.$emit('submitForm', {
+        group: this.group
       })
     },
   }
