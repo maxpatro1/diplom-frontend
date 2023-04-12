6d188abb87de231ffd3db462d00e3b61c3f99def
Daniil Denisov <Cap.den2001@gmail.com>
2023-04-12 11:32:33 +0300

    added ModalWrapper

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -1,28 +1,31 @@
 <template>
-  <v-form class="auth" @submit="submit">
-    <h1 class="auth__app-title">WebLab</h1>
-    <base-input
-      v-model="email"
-      name="email"
-      type="email"
-      placeholder="Введите почту"
-    />
-    <div class="auth__wrapper">
+  <modal-wrapper>
+    <v-form class="auth" @submit="submit">
+      <h1 class="auth__app-title">WebLab</h1>
       <base-input
-        v-model="password"
-        name="password"
-        type="password"
-        placeholder="Введите пароль"
+        v-model="email"
+        name="email"
+        type="email"
+        placeholder="Введите почту"
       />
-      <button type="button" class="auth__link">Забыли пароль?</button>
-    </div>
-    <base-button type="submit">Войти</base-button>
-  </v-form>
+      <div class="auth__wrapper">
+        <base-input
+          v-model="password"
+          name="password"
+          type="password"
+          placeholder="Введите пароль"
+        />
+        <button type="button" class="auth__link">Забыли пароль?</button>
+      </div>
+      <base-button type="submit">Войти</base-button>
+    </v-form>
+  </modal-wrapper>
 </template>
 
 <script>
 import BaseButton from '@/components/base/BaseButton.vue';
 import BaseInput from '@/components/base/BaseInput.vue';
+import ModalWrapper from '@/components/hoc/ModalWrapper.vue';
 import { Form } from 'vee-validate';
 
 export default {
@@ -30,6 +33,7 @@ export default {
   components: {
     BaseButton,
     BaseInput,
+    ModalWrapper,
     VForm: Form
   },
   data() {
@@ -48,14 +52,10 @@ export default {
 
 <style lang="scss" scoped>
 .auth {
+  width: 100%;
   display: grid;
   grid-template-columns: auto;
   gap: 24px;
-  width: 100%;
-  max-width: 384px;
-  padding: 32px;
-  background-color: #fff;
-  border-radius: 32px;
 
   &__wrapper {
     display: grid;
---
+++ src/components/hoc/ModalWrapper.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="modal-wrapper">
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ModalWrapper'
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-wrapper {
+  width: 100%;
+  max-width: 384px;
+  padding: 32px;
+  background-color: #fff;
+  border-radius: 32px;
+}
+</style>
