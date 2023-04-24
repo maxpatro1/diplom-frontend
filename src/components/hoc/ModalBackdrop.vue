6b0e172b0fdaf88d6d83e34dc89c342ed28aec29
Daniil Denisov <Cap.den2001@gmail.com>
2023-04-24 22:16:41 +0300

    added ModalBackdrop

---
+++ src/components/hoc/ModalBackdrop.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="backdrop" @keyup="$emit('keyupOnBackdrop')" @click="$emit('clickOnBackdrop')">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalBackdrop',
+};
+</script>
+
+<style lang="scss" scoped>
+.backdrop {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 100;
+  backdrop-filter: blur(12.5px);
+}
+</style>
