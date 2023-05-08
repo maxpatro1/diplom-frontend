01f18ac4e247c7e7c40a59c591057ed6085097f4
Daniil Denisov <Cap.den2001@gmail.com>
2023-05-08 20:06:00 +0300

    added Lab title

--- src/views/courses/_courseId/_labId/IndexView.vue
+++ src/views/courses/_courseId/_labId/IndexView.vue
@@ -1,7 +1,8 @@
 <template>
-  <lab-workplace>
+  <lab-workplace class="workplace">
     <div class="device">
-      device
+      <h1 class="device__title">{{ labData.title }}</h1>
+      <lab-bread-crumbs :breadCrumbs="breadCrumbs" />
     </div>
     <div class="toolbar">toolbar</div>
   </lab-workplace>
@@ -9,11 +10,38 @@
 
 <script>
 import LabWorkplace from '@/components/hoc/LabWorkplace.vue';
+import LabBreadCrumbs from '@/components/lab/LabBreadCrumbs.vue';
 
 export default {
   name: 'LabView',
   components: {
     LabWorkplace,
+    LabBreadCrumbs,
+  },
+  data() {
+    return {
+      labData: {
+        title: 'Лабораторная работа №2',
+      },
+      breadCrumbs: [
+        { title: 'Список курсов', link: '/' },
+        { title: 'ТмиЭЭлТ', link: '/' },
+        { title: 'Лабораторная работа №1', link: '/' },
+      ],
+    };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.workplace {
+  padding: 24px 0;
+}
+
+.device__title {
+  font-size: 24px;
+  line-height: 32px;
+  font-weight: 600;
+  margin-bottom: 8px;
+}
+</style>
