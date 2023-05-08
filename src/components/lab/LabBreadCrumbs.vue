86c5c7d232441e1f1931c68d551cdac9dc453b8c
Daniil Denisov <Cap.den2001@gmail.com>
2023-05-08 20:05:39 +0300

    added LabBreadCrumbs

---
+++ src/components/lab/LabBreadCrumbs.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bread-crumbs">
+    <template v-for="(crumb, idx) in breadCrumbs" :key="crumb">
+      <router-link class="bread-crumbs__link" :to="crumb.link">
+       {{ crumb.title }}
+      </router-link>
+      <span v-if="idx !== breadCrumbs.length - 1" class="bread-crumbs__slash">
+        &nbsp;/&nbsp;
+      </span>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LabBreadCrumbs',
+  props: {
+    breadCrumbs: {
+      type: Array,
+      required: true,
+      validator: (arr) => arr.length === arr.filter((el) => (el.title && el.link)).length,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.bread-crumbs {
+  color: $traffic-gray;
+  font-size: 12px;
+  line-height: 16px;
+  font-weight: 400;
+
+  &__link {
+    &:last-child {
+      color: $black-brown;
+    }
+  }
+}
+</style>
