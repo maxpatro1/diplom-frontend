8485b147b9f00e9a9d8bba9bb36cdb5bc9647366
Daniil Denisov <Cap.den2001@gmail.com>
2023-05-08 19:35:25 +0300

    added LabBackButton

---
+++ src/components/lab/LabBackButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="button">
+    <svg-sprite
+      symbol="arrow-light"
+      width="24"
+      height="24"
+      class="button__icon"
+    />
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'LabBackButton',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: 400;
+  background-color: transparent;
+
+  &__icon {
+    margin-right: 12px;
+    stroke: $black-brown;
+  }
+}
+</style>
